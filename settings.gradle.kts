11a809c531c635d91785f4f74a5432429ae4dc2b
ysera34 <ycyoon34@gmail.com>
2020-12-16 23:15:52 +0900

    spring boot test modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,9 @@
 rootProject.name = "data-collector"
 
 include("common")
+include("common-test")
 include("server")
+include("server-test")
 
 rootProject.children.forEach { project ->
     project.projectDir = file("subprojects/${project.name}")
